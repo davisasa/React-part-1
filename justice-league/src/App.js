dfe02d0ef4a708e9c75766f9de5d54137be0998f
davisasa <92169494+davisasa@users.noreply.github.com>
2024-03-28 20:34:19 +0000

    second commit

--- justice-league/src/App.js
+++ justice-league/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Justice-League
         </p>
         <a
           className="App-link"
